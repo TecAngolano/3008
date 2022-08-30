37967e58f12ec50470e1c91a12f55ebbe08eaa9d
Tept Do Vasco <84383797+TecAngolano@users.noreply.github.com>
2022-08-30 09:27:19 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const conexao = async()=>{
+    const atlas = await mongoose.connect("mongodb+srv://user3tri:user3tri@fiaptecnico.kreph.mongodb.net/html5up")    
+}
+const modelo = mongoose.Schema({
+    estilo:String,
+    imagem:String,
+    titulo:String,
+    texto:String,
+    publicado:{type:Date,default:Date.now}
+})
